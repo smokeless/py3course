38ef8d1fc06741e1ff6c910f2451370d8bd0ec75
Ross Russell <ross.russell.ca@gmail.com>
2017-12-01 11:32:38 -0800

    first init

---
+++ 01_Character_Input.py
@@ -0,0 +1,56 @@
+"""Create a program that asks the user to enter their name and their
+age. Print out a message addressed to them that tells them the year
+that they will turn 100 years old.
+
+Extras:
+
+Add on to the previous program by asking the user for another
+number and printing out that many copies of the previous message.
+(Hint: order of operations exists in Python)
+Print out that many copies of the previous message on separate lines.
+(Hint: the string "\n is the same as pressing the ENTER button)"""
+import datetime
+
+def checkInt(input):
+    try:
+        int(input)
+        return True
+    except ValueError:
+        return False
+
+def getYearAt100(age):
+    now = datetime.datetime.now()
+    currentYear = now.year
+    age = 100 - int(age)
+    oneHundred = currentYear + age
+    return oneHundred
+
+def printMessage(name, age):
+    age = getYearAt100(age)
+    print('Hey', name, 'you will be 100 in:', age )
+
+
+
+print('Please input your name.')
+name = input('>>')
+
+print('Please input your age.')
+age = input('>>')
+
+
+
+if checkInt(age):
+    printMessage(name, age)
+else:
+    print('Bad age input.')
+    exit(1)
+
+print('How many repeats would you like?')
+repeats = input('>>')
+
+if checkInt(repeats):
+    for i in range(int(repeats)):
+        printMessage(name, age)
+else:
+    print('Bad multiplier.')
+    exit(1)

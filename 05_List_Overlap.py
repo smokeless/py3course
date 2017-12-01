5e8751cb73483af0bbcc23342fd35d3875773b23
Ross Russell <ross.russell.ca@gmail.com>
2017-12-01 13:25:32 -0800

    Looks good.

--- 05_List_Overlap.py
+++ 05_List_Overlap.py
@@ -14,6 +14,9 @@ Randomly generate two lists to test this
 Write this in one line of Python (don’t worry if you can’t figure
 this out at this point - we’ll get to it soon)
 """
+import random
+
+random.seed()
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -26,7 +29,22 @@ def commonElements(a, b):
         if item in b:
             newList.append(item)
     newList = set(newList)
-    print(newList)
+    if not newList:
+        print('No common numbers.')
+    else:
+        print(newList)
 
+def getRandomList( size ):
+    iterations = random.randint(0, size)
+    newList = []
+    for i in range(iterations):
+        newList.append(random.randint(0, 100))
+    return newList
 
+commonElements(a, b)
+
+a = getRandomList(random.randint(1, 30))
+b = getRandomList(random.randint(1, 50))
+a.sort()
+b.sort()
 commonElements(a, b)

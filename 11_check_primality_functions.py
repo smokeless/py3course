0ba933897a678a539f3d130ddb48dfa188578ace
Ross Russell <ross.russell.ca@gmail.com>
2017-12-03 14:37:41 -0800

    primes

---
+++ 11_check_primality_functions.py
@@ -0,0 +1,31 @@
+"""
+Ask the user for a number and determine whether the number is
+prime or not. (For those who have forgotten, a prime number
+is a number that has no divisors.). You can (and should!)
+use your answer to Exercise 4 to help you.
+Take this opportunity to practice using functions, described below.
+
+"""
+
+def getInput():
+    print('Input a number to check for prime.')
+    uInput = input('>> ')
+    try:
+        int(uInput)
+    except ValueError:
+        print('invalid input')
+    return int(uInput)
+
+def isPrime(number):
+    for i in range(2, (number-1 )):
+        if number % i == 0:
+            print(number, 'divisible by', i)
+            return False
+    return True
+
+
+uInput = getInput()
+if isPrime(uInput):
+    print("It's prime.")
+else:
+    print("It's not prime.")

9dcff882841428b3c039828c1dc47d9b75cc626f
Ross Russell <ross.russell.ca@gmail.com>
2017-12-12 17:50:36 -0800

    first

--- slotMachine.py
+++ slotMachine.py
@@ -1,6 +1,6 @@
 import random
 
-def getNumbers(difficulty):
+def getRoll(difficulty=3):
     symbols = ['!', '#', '$', '&', '*']
     selections = []
     for i in range(difficulty):
@@ -8,8 +8,21 @@ def getNumbers(difficulty):
 
     return selections
 
-yourRoll = getNumbers(30)
+def checkRoll(roll):
+    for i in roll:
+        if i == roll[0]:
+            return True
+        else:
+            return False
+
+yourRoll = getNumbers(3)
 
 for i in yourRoll:
     print(i, end=' ')
+
+if checkRoll(yourRoll):
+    print('You win!')
+else:
+    print('You lose, deleting all files.')
+
 
